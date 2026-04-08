e8b9de16ba692e033de8b3fb544920deb016096a
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-07 21:12:22 -0300

    tempWater.java

--- tempWater.java
+++ tempWater.java
@@ -4,17 +4,20 @@ public class tempWater {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        for(int i = 0; i < 12; i++) {
-            System.out.println("Digite a temperatura: ");
+        for(int i = 1; i <= 12; i++) {
+            System.out.println("Digite a temperatura" + i ": ");
             double temp = teclado.nextDouble();
             for (int j = 1; temp < 4 && temp > 10; j++) {
                 System.out.println("Digite a temperatura: ");
                 temp = teclado.nextDouble();
-            } 
+            }
+            if (temp >= 4 || temp <= 10) {
+                double soma += temp;
+                double media = soma / 12;
+                System.out.println("A média de temperatura de hoje é " + media + " graus Celsius");
+            }
         }
-        double soma += temp;
-        double media = soma / 12;
-        System.out.println("A média de temperatura de hoje é " + media + " graus Celsius");
+        
 
         teclado.close();
     }
